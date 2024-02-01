a109d0f901af2b7ad0cd09c8bc50cc9827020b26
SaLuTeK1 <138734652+SaLuTeK1@users.noreply.github.com>
2024-02-01 19:31:40 +0200

    Simspons

--- src/App.js
+++ src/App.js
@@ -46,61 +46,6 @@ const App = () => {
             img: 'https://upload.wikimedia.org/wikipedia/ru/9/9d/Maggie_Simpson.png'
         }]
 
-    let cartoons = [
-        {
-            "id": 1,
-            "name": "Rick Sanchez",
-            "status": "Alive",
-            "species": "Human",
-            "gender": "Male",
-            "image": "https://rickandmortyapi.com/api/character/avatar/1.jpeg",
-        },
-        {
-            "id": 2,
-            "name": "Morty Smith",
-            "status": "Alive",
-            "species": "Human",
-            "type": "",
-            "gender": "Male",
-            "image": "https://rickandmortyapi.com/api/character/avatar/2.jpeg",
-        },
-        {
-            "id": 3,
-            "name": "Summer Smith",
-            "status": "Alive",
-            "species": "Human",
-            "type": "",
-            "gender": "Female",
-            "image": "https://rickandmortyapi.com/api/character/avatar/3.jpeg",
-        },
-        {
-            "id": 4,
-            "name": "Beth Smith",
-            "status": "Alive",
-            "species": "Human",
-            "type": "",
-            "gender": "Female",
-            "image": "https://rickandmortyapi.com/api/character/avatar/4.jpeg",
-        },
-        {
-            "id": 5,
-            "name": "Jerry Smith",
-            "status": "Alive",
-            "species": "Human",
-            "type": "",
-            "gender": "Male",
-            "image": "https://rickandmortyapi.com/api/character/avatar/5.jpeg",
-        },
-        {
-            "id": 6,
-            "name": "Abadango Cluster Princess",
-            "status": "Alive",
-            "species": "Alien",
-            "type": "",
-            "gender": "Female",
-            "image": "https://rickandmortyapi.com/api/character/avatar/6.jpeg",
-        }
-    ]
     return (
         <div>
             {simpsons.map(simpson=> <Simpson simspon={simpson}/>)}
